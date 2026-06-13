b9decf509eb39ed5281d09d67c1cd8e1d94f961a
Fabio Ruan <fabioruanmoreiradealencar@gmail.com>
2026-06-13 17:36:51 -0300

    fix: corrige violações de checkstyle no DatabaseConnection

--- src/main/java/com/medtrack/db/DatabaseConnection.java
+++ src/main/java/com/medtrack/db/DatabaseConnection.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    // Se a variável DB_URL não existir (rodando local), ele usa o banco da máquina de vocês.
-    private static final String DEFAULT_URL = "jdbc:postgresql://localhost:5432/medtrack?user=postgres&password=sua_senha_local";
-    
-    // Lê da nuvem, se for nulo, usa o padrão local
-    private static final String URL = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : DEFAULT_URL;
+    private static final String DEFAULT_URL =
+            "jdbc:postgresql://localhost:5432/medtrack?user=postgres&password=sua_senha_local";
+
+    private static final String URL =
+            System.getenv("DB_URL") != null ? System.getenv("DB_URL") : DEFAULT_URL;
 
     public static Connection getConnection() throws SQLException {
         try {
